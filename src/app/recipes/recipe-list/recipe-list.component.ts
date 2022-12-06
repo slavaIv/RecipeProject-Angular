4bfd8aabaa9997b7c94e49e2757000ec5db61060
slavaIv <slavchiky@gmail.com>
2022-12-07 00:45:20 +0100

    component were added, model example was added

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe-model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+
+export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+        new Recipe("A test recipe", "Just description", "")
+    ];
+
+    constructor() {
+
+    }
+
+    ngOnInit() {
+
+    }
+}
