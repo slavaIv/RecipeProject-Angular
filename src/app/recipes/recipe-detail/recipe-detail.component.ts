6946833edec111538e5d9131f7df829483e3fb31
slavaIv <slavchiky@gmail.com>
2022-12-13 23:25:54 +0100

    router was added

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,23 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Recipe } from '../recipe-model';
 import { RecipeService } from '../recipe.service';
 
 @Component({
-  selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+    selector: 'app-recipe-detail',
+    templateUrl: './recipe-detail.component.html',
+    styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
 
-    @Input() recipe: Recipe;
-
-    constructor(private recipeService: RecipeService) { }
+    id: number;
+    recipe: Recipe;
+    
+    constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) { }
 
     onAddToShoppingList() {
         this.recipeService.addIngridientsToShoppingList(this.recipe.ingridients);
     }
 
+    onEditRecipe() {
+        this.router.navigate(["edit"], {relativeTo: this.route})
+    }
+
     ngOnInit() {
-        
+        this.route.params.subscribe(
+            (params: Params) => {
+                this.id = +params["id"];
+                this.recipe = this.recipeService.getRecipeOnId(this.id);
+            }
+        );
     }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,11 +10,11 @@ export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe("Schnitzel", "Tasty Schnitzel", "https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_1280.png", [
+        new Recipe("Schnitzel", "Tasty Schnitzel", "/assets/schnipo-g87de26108_1920.jpg", [
             new Ingridient("Meat", 1),
             new Ingridient("French Fries", 20)
         ]),
-        new Recipe("Burger", "Tasty Burger", "https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_1280.png", [
+        new Recipe("Burger", "Tasty Burger", "/assets/hamburger-g7d0da8677_1920.jpg", [
             new Ingridient("Buns", 2),
             new Ingridient("Meat", 1)
         ]),
@@ -29,4 +29,9 @@ export class RecipeService {
     addIngridientsToShoppingList(ingridients: Ingridient[]) {
         this.shoppingList.addIngridients(ingridients);
     }
+
+    getRecipeOnId(id: number): Recipe {
+        return this.recipes[id];
+    }
+
 }
